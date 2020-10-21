5e62df611e042e12e48fb495d416f2db9d98db78
RH8113 <poker-_-face-8113@outlook.com>
2020-10-21 16:34:45 +0900

    ファイル移動

---
+++ Assets/CoinCotroller.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CoinCotroller : MonoBehaviour
+{
+    void Start()
+    {
+        this.transform.Rotate(0, Random.Range(0, 360), 0);
+    }
+
+    void Update()
+    {
+        this.transform.Rotate(0, 3, 0);
+        
+    }
+}
---
+++ Assets/DestroyController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class DestroyController : MonoBehaviour
+{
+    private float deadLine = -1;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(transform.position.z < this.deadLine)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
